f6cd39bfad691da6a45e2608a6c5ab37212ee149
shimomurayuushi <youshixiacun@gmail.com>
2023-06-29 06:30:38 +0900

    'change views.py'

--- calculator/views.py
+++ calculator/views.py
@@ -7,13 +7,13 @@ import numpy as np
 def index(request):
     params = {
         'title': 'Calculator45',
-        'forms': CalcPlusForm,
+        'forms': CalcPlusForm(),
         'Answer': 'Answer is '
     }
     if (request.method == 'POST'):
         cal1 = np.square(float(request.POST['val1']) - float(request.POST['val2']))
         cal2 = np.square(float(request.POST['val3']) - float(request.POST['val4']))
-        params['answer'] = 'Answer is ' + str(np.sqrt(cal1 + cal2) * 1.4) 
+        params['answer'] = '芯引き前の寸法 ' + str(np.sqrt(cal1 + cal2) * 1.4) 
         params['forms'] = CalcPlusForm(request.POST)
     return render(request, 'calculator/index.html', params)
 
